76fc44aa5cb41ff1a4e35caa90d2db6aab2b617c
Ori Hillel <orihillel@campus.technion.ac.il>
2023-05-31 18:26:09 +0300

    Date and DateTime getter/setter classes' methods implemented

--- src/Date.java
+++ src/Date.java
@@ -8,7 +8,29 @@ public class Date {
     private final int MAX_MONTH = 12;
     private final int MIN_YEAR = -3999;
     private final int MAX_YEAR = 3999;
-    public Date(int day, int month, int year){
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+    public void setDay(int day) {
+        if (day >= MIN_DAY && day <= MAX_DAY)  this.day = day;
+    }
+
+    public void setMonth(int month) {
+        if (month >= MIN_MONTH && month <= MAX_MONTH)  this.month = month;
+    }
+
+    public void setYear(int year) {
+        if (year >= MIN_YEAR && year <= MAX_YEAR) this.year = year;
+    }
+    public Date(int year, int month, int day){
         if (day >= MIN_DAY && day <= MAX_DAY) this.day = day;
         if (month >= MIN_MONTH && month <= MAX_MONTH) this.month = month;
         if (year >= MIN_YEAR && year <= MAX_YEAR) this.year = year;
--- src/DateTime.java
+++ src/DateTime.java
@@ -1,4 +1,22 @@
 public class DateTime extends Date{
     private int hour;
     private int minute;
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public DateTime(int day, int month, int year,)
 }
