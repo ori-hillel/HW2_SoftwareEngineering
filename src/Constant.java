5e66281247a071b01d33ae42689dde1fed05eb84
Ori Hillel <orihillel@campus.technion.ac.il>
2023-06-01 16:39:38 +0300

    Constant class created, not fully implemented

---
+++ src/Constant.java
@@ -0,0 +1,33 @@
+public class Constant extends Function {
+    double value;
+    public Constant(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public double valueAt(double x) {
+        return this.value;
+    }
+@Override
+    public Function derivative() { return new Constant(0);}
+@Override
+    public Function taylorPolynomial(int n) {
+        return new Constant(0);
+}
+
+    @Override
+    public double bisectionMethod(double a, double b, double epsilon) {
+        return 0;
+    }
+
+    @Override
+    public double newtonRaphsonMethod(double a, double epsilon) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+    p
+}
