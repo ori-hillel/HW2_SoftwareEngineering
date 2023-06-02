b238d0fa86ced5f0e9a5c2de0a34b421ea598eb6
Ori Hillel <orihillel@campus.technion.ac.il>
2023-06-02 12:54:15 +0300

    MultiProduct's and MultiSum's constructors updated s.t. sending 1 argument would lead to compilation error

--- src/MultiProduct.java
+++ src/MultiProduct.java
@@ -1,6 +1,6 @@
 public class MultiProduct extends Function {
     Function[] function;
-    public MultiProduct(Function ... functions) {
+    public MultiProduct(Function function, Function ... functions) {
     }
 
 }
--- src/MultiSum.java
+++ src/MultiSum.java
@@ -1,4 +1,4 @@
 public class MultiSum extends Function {
     protected Function[] functions;
-    public MultiSum(Function ... functions) {}
+    public MultiSum(Function function, Function ... functions) {}
 }
