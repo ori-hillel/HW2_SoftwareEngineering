177ff86e791f263003b00e7c9c0a67b09d0eceae
Ori Hillel <orihillel@campus.technion.ac.il>
2023-06-02 12:58:44 +0300

    Minor changes

--- src/MultiProduct.java
+++ src/MultiProduct.java
@@ -3,4 +3,33 @@ public class MultiProduct extends Function {
     public MultiProduct(Function function, Function ... functions) {
     }
 
+    @Override
+    public double valueAt(double x) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public Function derivative() {
+        return null;
+    }
+
+    @Override
+    public double bisectionMethod(double a, double b, double epsilon) {
+        return 0;
+    }
+
+    @Override
+    public double newtonRaphsonMethod(double a, double epsilon) {
+        return 0;
+    }
+
+    @Override
+    public Function taylorPolynomial(int n) {
+        return null;
+    }
 }
--- src/MultiSum.java
+++ src/MultiSum.java
@@ -1,4 +1,34 @@
 public class MultiSum extends Function {
     protected Function[] functions;
     public MultiSum(Function function, Function ... functions) {}
+
+    @Override
+    public double valueAt(double x) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public Function derivative() {
+        return null;
+    }
+
+    @Override
+    public double bisectionMethod(double a, double b, double epsilon) {
+        return 0;
+    }
+
+    @Override
+    public double newtonRaphsonMethod(double a, double epsilon) {
+        return 0;
+    }
+
+    @Override
+    public Function taylorPolynomial(int n) {
+        return null;
+    }
 }
