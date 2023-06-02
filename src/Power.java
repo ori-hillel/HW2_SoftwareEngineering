10771feceaee8683dc5870a4cd5db9211d1fe2c4
Ori Hillel <orihillel@campus.technion.ac.il>
2023-06-02 16:35:32 +0300

    Quotient

---
+++ src/Power.java
@@ -0,0 +1,32 @@
+public class Power extends Function {
+    protected int power;
+    @Override
+    public double valueAt(double x) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public Function derivative() {
+        return null;
+    }
+
+    @Override
+    public double bisectionMethod(double a, double b, double epsilon) {
+        return 0;
+    }
+
+    @Override
+    public double newtonRaphsonMethod(double a, double epsilon) {
+        return 0;
+    }
+
+    @Override
+    public Function taylorPolynomial(int n) {
+        return null;
+    }
+}
---
+++ src/Quotient.java
@@ -0,0 +1,33 @@
+public class Quotient extends Function {
+Function function;
+    public Quotient(Function function) {}
+    @Override
+    public double valueAt(double x) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public Function derivative() {
+        return null;
+    }
+
+    @Override
+    public double bisectionMethod(double a, double b, double epsilon) {
+        return 0;
+    }
+
+    @Override
+    public double newtonRaphsonMethod(double a, double epsilon) {
+        return 0;
+    }
+
+    @Override
+    public Function taylorPolynomial(int n) {
+        return null;
+    }
+}
