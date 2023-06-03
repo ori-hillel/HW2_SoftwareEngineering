90f58bc5d906fe4e342b933305f21e1b1519c775
Ori Hillel <orihillel@campus.technion.ac.il>
2023-06-03 16:43:58 +0300

    Minor updates

--- src/MultiSum.java
+++ src/MultiSum.java
@@ -8,7 +8,7 @@ public class MultiSum extends Function {
     public MultiSum(Function function, Function ... functions) {
         this.functions = new Function[functions.length + 1];
         this.functions[0] = function;
-        for (int i = 0; i < this.functions.length; i++)
+        for (int i = 0; i < functions.length; i++)
             this.functions[i+1] = functions[i];
     }
 
