216e72b30059a1cf09bfaf97e5221b9471772ea5
Ori Hillel <orihillel@campus.technion.ac.il>
2023-06-05 23:57:03 +0300

    In progress (3)...

--- src/Negation.java
+++ src/Negation.java
@@ -3,8 +3,17 @@ public class  Negation extends Product {
     public Negation(Function function) {
         super(new Constant(-1), function);
     }
+    public Function getFunc() {
+        return super.getFuncB();
+    }
     @Override
     public String toString() {
         return "(-" + super.getFuncB().toString() + ")";
     }
+    @Override
+    public Function derivative() {
+        return new Negation(getFuncB());
+    }
+
+
 }
